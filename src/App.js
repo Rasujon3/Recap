27f82e191897c6befc6db67f07687b8aabdd6f70
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-23 23:55:20 +0600

    end of 33

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import react , {useEffect, useState} from 'react';
+
 
 function App() {
+  const [nayoks, setNayoks] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res =>res.json())
+    .then(data=> setNayoks(data));
+  },[])
+
+  // const nayoks = [{name:'Jasim',age:56},{name:'Jasim',age:56},
+  // {name:'Bappa',age:56},{name:'Alam',age:56},{name:'Jasim',age:56}];
+  nayoks.map(nayok => console.log(nayok));
   return (
     <div className="App">
-      <Nayok></Nayok>
-      <Nayok></Nayok>
-      <Nayok></Nayok>
-      <Nayok></Nayok>
-      <Nayok></Nayok>
+      <MovieCounter></MovieCounter>
+      {
+        nayoks.map(nk => <Nayok name={nk.name} age={nk.age} key={nk.id}></Nayok>)
+      }
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         
       </header>
     </div>
   );
 }
 
-function Nayok() {
-  return <div style={{border:'2px solid red'}}>
-    <h1>Ami KholNayak</h1>
-    <h3>I have done 5 movies</h3>
+function MovieCounter() {
+  let [count, setCount] = useState(0);
+
+  const handleClick = ()=> setCount(count +1 );
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add movie</button>
+      <h3>Number of movies: {count}</h3>
+      <MovieDisplay movies={count}></MovieDisplay>
+    </div>
+  )
+}
+
+function MovieDisplay(props) {
+  return <h4>Movies i hava acted: {props.movies}</h4>
+}
+
+function Nayok(props) {
+  console.log(props);
+  return (
+  <div style={{border:'2px solid red',margin:'20px'}}>
+    <h1>Ami Nayak-{props.name}</h1>
+    <h3>I have done 5 movies in {props.age || 30} years</h3>
   </div>
+  )
 }
 
 export default App;
