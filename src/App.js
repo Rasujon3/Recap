a204c292de8fdaeb7c28d0430dfab12636aee9aa
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-23 14:26:23 +0600

    End of 4 no video
    End of 4 no video

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Nayok></Nayok>
+      <Nayok></Nayok>
+      <Nayok></Nayok>
+      <Nayok></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
     </div>
   );
+}
+
+function Nayok() {
+  return <div style={{border:'2px solid red'}}>
+    <h1>Ami KholNayak</h1>
+    <h3>I have done 5 movies</h3>
+  </div>
 }
 
 export default App;
